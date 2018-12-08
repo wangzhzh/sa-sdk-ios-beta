fdda0d2594d2d6fac84d3065ab9bbc8549303bd9
王灼洲 <wangzhuozhou@sensorsdata.cn>
2018-12-08 13:10:22 +0800

    Release 1.8.29

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -35,7 +35,7 @@
 #import "SensorsAnalyticsExceptionHandler.h"
 #import "SAServerUrl.h"
 #import "SAAppExtensionDataManager.h"
-#define VERSION @"1.8.28"
+#define VERSION @"1.8.29"
 
 #define PROPERTY_LENGTH_LIMITATION 8191
 
